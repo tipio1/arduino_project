6808cecb89a5c5932e408f0496036f73c532f048
tipio <benjamin.rossier@yahoo.fr>
2021-10-06 17:06:29 +0200

    api_express

---
+++ api_express/test.js
@@ -0,0 +1,22 @@
+/*
+Oct 21 - author : benj
+test with hello world
+*/
+const express = require('express')
+const app = express()
+const port = 3001
+
+
+app.get('/metrics', (req, res) => {
+  res.send('Hello express test!')
+});
+
+// app.use('/metrics', express.static('/home/pi/express_apps/metrics'));
+
+/*app.get('/', (req, res) => {
+  res.send('Hello test express!');
+});*/
+
+app.listen(port, () => {
+  console.log(`Test app listening at http://localhost:${port}`)
+})
